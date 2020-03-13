34a8d03ff1a1f93d38f098ee654c3fbcf4ce1d25
Teemu Koponen <koponen@styra.com>
2020-03-13 10:44:44 -0700

    Include the prebuilt 0.28.4 libraries for Linux and Mac OS X.
    In addition, don't use pkgconfig to drive the C compiler and
    linker. This is to avoid the need to have the full platform-specific
    libgit2 installation available, either in tree or elsewhere.
    
    The go dummies are required in the header directories. If they are not
    there the golang vendoring won't bring in the header files and they
    are required to compile the go code with cgo imports.

---
+++ git_styra.go
@@ -0,0 +1,18 @@
+package git
+
+import (
+	_ "github.com/libgit2/git2go/v28/git2"
+	_ "github.com/libgit2/git2go/v28/git2/sys"
+)
+
+/*
+#include <git2.h>
+#cgo darwin,amd64 LDFLAGS: -L${SRCDIR} -lgit2-darwin-amd64 -framework CoreFoundation -framework Security -lz -liconv
+#cgo linux,amd64 LDFLAGS: -L${SRCDIR} -lgit2-linux-amd64 -lssl -lcrypto -lz -lpthread -ldl
+
+#if LIBGIT2_VER_MAJOR != 0 || LIBGIT2_VER_MINOR != 28
+# error "Invalid libgit2 version; this git2go supports libgit2 v0.28"
+#endif
+
+*/
+import "C"
